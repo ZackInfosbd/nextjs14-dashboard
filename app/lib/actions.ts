58b747bdf693ab83abb7dbcd3684de9fa015f309
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-01-03 18:35:14 +0100

    ci: validate and prepare the data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,14 +1,33 @@
 'use server';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['paid', 'pending']),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  //   const rawFormData = {
+  //     customerId: formData.get('customerId'),
+  //     amount: formData.get('amount'),
+  //     status: formData.get('status'),
+  //   };
 
   //   to handle multiple Form inputs
   // const rawFormData = Object.fromEntries(formData.entries());
 
-  console.log(rawFormData);
+  //   console.log(rawFormData);
 }
